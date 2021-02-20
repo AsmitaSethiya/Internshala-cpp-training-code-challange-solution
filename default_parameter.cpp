56a7746a8455209dda43d63e423b2743ce3ca7f6
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-02-20 19:53:56 +0530

    Create default_parameter.cpp

---
+++ default_parameter.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string getFullName(string, string);
+
+int main() {
+
+	bool isCodingFun = true;
+	if ( isCodingFun )
+	{
+		cout << "Let's code more.\n";
+	}
+	else
+	{
+		cout << "Find another job.\n";
+	}
+
+	int x = 10,  y =20;
+	bool b1 = x > y;
+	bool b2 = x < y;
+	bool b3 = x == y;
+
+	cout << "b1:" << b1 << endl;
+	cout << "b2:" << b2 << endl;
+	cout << "b3:" << b3 << endl;
+
+	return 0;
+}
