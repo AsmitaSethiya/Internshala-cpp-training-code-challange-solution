95e461548dd7a24cacce4ce9ab3994d49c4cc822
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-02-20 14:06:21 +0530

    Create code-challange.cpp

---
+++ code-challange.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	//print statements using endl keyword
+	cout << "My name is Asmita Sethiya." << endl << "I am 20 years old." << endl;
+
+	// printf statement using \n
+	cout << "My name is Asmita Sethiya.\nI am 20 years old.";
+  
+  
+  // cout << "My name is Asmita Sethiya.\n << "I am 20 years old.";
+
+	return 0;
+}
