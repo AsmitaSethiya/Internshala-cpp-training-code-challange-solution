d2c8af1c0e4f9b5f97f232aedec6518704a10a99
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-02-20 14:14:10 +0530

    Create code-challange2.cpp

---
+++ code-challange2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string country = "India";
+	char s = '$';
+	float gdp = 2.2;
+	int year = 2015;
+
+	cout << "As economic reform picked up the pace, " << country << "'s GDP grew five-fold to reach US" << s << gdp << " trillion in "<< year << " (as per IMF estimates.)";
+
+
+	return 0;
+}
