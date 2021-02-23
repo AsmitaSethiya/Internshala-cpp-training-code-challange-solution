8db5b1c8838eeda55ab2fd7363fbd63bc1e44eda
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-02-23 23:45:12 +0530

    Update macro.cpp

--- macro.cpp
+++ macro.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-#define RECTANGLE(length, breadth) (length * breadth)
+#define RECTANGLE(length, breadth) (length * breadth)  // macro with parameter
 
 int main()
 {
-
-	cout << "Area: " << RECTANGLE(5,2);
+	// Find the area of rectangle
+	cout << "Area: " << RECTANGLE(5,2);  
 
 }
